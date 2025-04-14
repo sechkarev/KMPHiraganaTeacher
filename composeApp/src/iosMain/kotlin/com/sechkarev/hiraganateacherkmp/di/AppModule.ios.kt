d09f5699b3371f06a48a911a33b3d9c95fbaded3
Vladimir Sechkarev <sechkarevs@gmail.com>
2025-04-14 17:02:21 +0200

    add text recognizer to koin on ios

--- composeApp/src/iosMain/kotlin/com/sechkarev/hiraganateacherkmp/di/AppModule.ios.kt
+++ composeApp/src/iosMain/kotlin/com/sechkarev/hiraganateacherkmp/di/AppModule.ios.kt
@@ -2,6 +2,7 @@ package com.sechkarev.hiraganateacherkmp.di
 
 import com.sechkarev.hiraganateacherkmp.database.ChallengeSolutionDatabase
 import com.sechkarev.hiraganateacherkmp.getDatabase
+import com.sechkarev.hiraganateacherkmp.textrecognition.TextRecognizer
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -9,4 +10,5 @@ actual val platformModule: Module =
     module {
         single { getDatabase() }
         factory { get<ChallengeSolutionDatabase>().challengeSolutionDao() }
+        single { TextRecognizer() }
     }
