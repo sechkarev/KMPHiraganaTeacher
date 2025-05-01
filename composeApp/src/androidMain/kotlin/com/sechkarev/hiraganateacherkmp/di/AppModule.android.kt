5af9d2cf0186ce81ec0495ea53573fe9d091e653
Vladimir Sechkarev <sechkarevs@gmail.com>
2025-05-01 09:53:29 +0200

    get rid of text recognizer 1

--- composeApp/src/androidMain/kotlin/com/sechkarev/hiraganateacherkmp/di/AppModule.android.kt
+++ composeApp/src/androidMain/kotlin/com/sechkarev/hiraganateacherkmp/di/AppModule.android.kt
@@ -1,9 +1,7 @@
 package com.sechkarev.hiraganateacherkmp.di
 
-import com.google.mlkit.common.model.RemoteModelManager
 import com.sechkarev.hiraganateacherkmp.database.ChallengeSolutionDatabase
 import com.sechkarev.hiraganateacherkmp.getDatabase
-import com.sechkarev.hiraganateacherkmp.textrecognition.TextRecognizer
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -11,7 +9,4 @@ actual val platformModule: Module =
     module {
         single<ChallengeSolutionDatabase> { getDatabase(context = get()) }
         factory { get<ChallengeSolutionDatabase>().challengeSolutionDao() }
-        single {
-            TextRecognizer(RemoteModelManager.getInstance())
-        }
     }
--- composeApp/src/iosMain/kotlin/com/sechkarev/hiraganateacherkmp/di/AppModule.ios.kt
+++ composeApp/src/iosMain/kotlin/com/sechkarev/hiraganateacherkmp/di/AppModule.ios.kt
@@ -2,7 +2,6 @@ package com.sechkarev.hiraganateacherkmp.di
 
 import com.sechkarev.hiraganateacherkmp.database.ChallengeSolutionDatabase
 import com.sechkarev.hiraganateacherkmp.getDatabase
-import com.sechkarev.hiraganateacherkmp.textrecognition.TextRecognizer
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -10,5 +9,4 @@ actual val platformModule: Module =
     module {
         single { getDatabase() }
         factory { get<ChallengeSolutionDatabase>().challengeSolutionDao() }
-        single { TextRecognizer() }
     }
